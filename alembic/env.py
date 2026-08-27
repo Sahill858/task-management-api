51d97e95747097acbf6ea3cd7f43103e195500da
Sahil <sahil.sahu@cloudbrx.in>
2026-08-27 16:29:28 +0530

    Added a import for refreshtokens

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from app.database import Base
 
 from app.models.task import Task
 from app.models.user import User
+from app.models.refresh_token import RefreshToken
 
 # Alembic Config object
 config = context.config
